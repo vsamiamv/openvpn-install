42bd6114644b0a84e9bbbb38c0ffa4042327c5fc
vsamiamv <63832288+vsamiamv@users.noreply.github.com>
2022-12-28 14:00:32 -0500

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -727,7 +727,7 @@ function installOpenVPN() {
 
 		echo "set_var EASYRSA_REQ_CN $SERVER_CN" >>vars
 		echo "set_var EASYRSA_CA_EXPIRE 7300">>vars
-        echo "set_var EASYRSA_CERT_EXPIRE 3650">>var
+        echo "set_var EASYRSA_CERT_EXPIRE 3650">>vars
 
 		# Create the PKI, set up the CA, the DH params and the server certificate
 		./easyrsa init-pki
